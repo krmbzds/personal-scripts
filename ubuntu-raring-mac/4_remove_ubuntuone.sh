dc1e8a2ec51c1115372e8b1aaf5699050299c1b7
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-13 18:40:07 +0300

    Add 4_remove_ubuntuone.sh

---
+++ ubuntu-raring-mac/4_remove_ubuntuone.sh
@@ -0,0 +1,18 @@
+
+# Kill processes
+
+sudo killall ubuntuone-login ubuntuone-preferences ubuntuone-syncdaemon
+
+
+# Remove related packages
+
+sudo apt-get purge ubuntuone-client python-ubuntuone-storage*
+
+
+# Commit genocide
+
+sudo rm -rf ~/.local/share/ubuntuone
+sudo rm -rf ~/.cache/ubuntuone
+sudo rm -rf ~/.config/ubuntuone
+sudo rm -rf ~/Ubuntu\ One
+
