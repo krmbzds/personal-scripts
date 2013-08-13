79aab0c5d44721756d6dcfd909721230f105387f
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-13 17:08:03 +0300

    Add -y parameter to apt-get lines

--- ubuntu-precise-mac/0_create_maintenance.sh
+++ ubuntu-precise-mac/0_create_maintenance.sh
@@ -1,12 +1,11 @@
-
 # Creating an easy to run update script
 
 sudo touch /usr/bin/maintenance
 
-echo "sudo apt-get update"     | sudo tee -a /usr/bin/maintenance
-echo "sudo apt-get upgrade"    | sudo tee -a /usr/bin/maintenance
-echo "sudo apt-get autoclean"  | sudo tee -a /usr/bin/maintenance
-echo "sudo apt-get autoremove" | sudo tee -a /usr/bin/maintenance
+echo "sudo apt-get -y update"     | sudo tee -a /usr/bin/maintenance
+echo "sudo apt-get -y upgrade"    | sudo tee -a /usr/bin/maintenance
+echo "sudo apt-get -y autoclean"  | sudo tee -a /usr/bin/maintenance
+echo "sudo apt-get -y autoremove" | sudo tee -a /usr/bin/maintenance
 
 sudo chmod +x /usr/bin/maintenance
 
