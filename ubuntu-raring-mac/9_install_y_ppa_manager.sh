649ce35765ce5784ba1c7077f648e2589e2528af
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-19 12:08:31 +0300

    Add 9_install_y_ppa_manager.sh

---
+++ ubuntu-raring-mac/9_install_y_ppa_manager.sh
@@ -0,0 +1,15 @@
+
+# Add PPA:
+
+sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+
+
+# apt-get update:
+
+sudo apt-get update
+
+
+# Install y-ppa-manager:
+
+sudo apt-get install -y y-ppa-manager
+
