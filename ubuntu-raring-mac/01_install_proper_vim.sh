232db3acc68e5532b56b9b91df840894a80a9807
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-22 15:34:01 +0300

    Add -y parameter to apt-get

--- ubuntu-raring-mac/01_install_proper_vim.sh
+++ ubuntu-raring-mac/01_install_proper_vim.sh
@@ -1,7 +1,6 @@
 
-
 # Delete vim-tiny and install a proper version
 
-sudo apt-get purge vim-common vim-tiny
-sudo apt-get install vim
+sudo apt-get -y purge vim-common vim-tiny
+sudo apt-get -y install vim
 
