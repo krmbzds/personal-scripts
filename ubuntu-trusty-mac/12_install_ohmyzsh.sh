bfe990a8c4d3750401318ebcf3f556ecafe6a4ad
Kerem Bozdas <krmbzds.github@gmail.com>
2014-05-17 03:10:17 +0300

    Add script to install ZSH and Oh-My-Zsh

---
+++ ubuntu-trusty-mac/12_install_ohmyzsh.sh
@@ -0,0 +1,19 @@
+# Install git
+
+sudo apt-get -y install git
+
+
+# Install zsh
+
+sudo apt-get -y install zsh
+
+
+# Download and run Oh-My-Zsh installtion script
+
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh && sh install.sh
+
+
+# Remove leftover
+
+sudo rm -f ./install.sh
+
