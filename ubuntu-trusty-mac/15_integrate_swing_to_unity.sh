f223fe8c4143f7ed2f37687a829988922408322f
Kerem Bozdas <krmbzds.github@gmail.com>
2014-05-17 12:53:28 +0300

    Add script to enable HUD integration for Java Swing Applications

---
+++ ubuntu-trusty-mac/15_integrate_swing_to_unity.sh
@@ -0,0 +1,17 @@
+# Add PPA
+
+sudo add-apt-repository -y ppa:danjaredg/jayatana
+
+
+# Re-synchronize package indexes
+
+sudo apt-get update
+
+
+# Install JAyatana
+
+sudo apt-get -y install jayatana
+
+
+# Java Swing Ayatana is a project that brings Ubuntu global
+# menu (AppMenu) and HUD support for Java Swing applications.
