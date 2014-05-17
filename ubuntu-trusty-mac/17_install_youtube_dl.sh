6d86994b83c595fd2982b367a89e3584e6fefcf0
Kerem Bozdas <krmbzds.github@gmail.com>
2014-05-17 13:38:20 +0300

    Add script to install youtube-dl

---
+++ ubuntu-trusty-mac/17_install_youtube_dl.sh
@@ -0,0 +1,9 @@
+# Download the latest build
+
+sudo wget http://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
+
+
+# Give execution permissions
+
+sudo chmod a+x /usr/local/bin/youtube-dl
+
