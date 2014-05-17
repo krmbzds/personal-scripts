9c8cc5d1cee1f74c263a0dc4e900e52d9cc0c360
Kerem Bozdas <krmbzds.github@gmail.com>
2014-05-17 13:22:54 +0300

    Add script to install Sublime Text 3

---
+++ ubuntu-trusty-mac/16_install_sublime_text.sh
@@ -0,0 +1,14 @@
+# Add PPA
+
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+
+
+# Re-synchronize package indexes
+
+sudo apt-get update
+
+
+# Get packages & start installation
+
+sudo apt-get -y install sublime-text-installer
+
